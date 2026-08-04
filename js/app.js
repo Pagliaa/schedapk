d9dd2c852f736a8685387b74d5e2efd18ef233d4
Pagliaa <pagliarin.davide@gmail.com>
2026-08-05 00:34:25 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 let TYPE_CHART = {};
 let ranks = [];
 
+// Change input
+// Select the form or a container wrapping your inputs
+const form = document.querySelector('form'); // or document.body / #my-container
+
+// 1. Tracks typing in inputs, textareas, number fields in real time
+form.addEventListener('input', (event) => {
+  const target = event.target;
+  
+  if (target.type !== 'checkbox' && target.type !== 'radio') {
+    console.log(`Input Modified [${target.name || target.id}]:`, target.value);
+  }
+});
+
+// 2. Tracks checkboxes, radio buttons, select dropdowns, and date pickers on toggle/selection
+form.addEventListener('change', (event) => {
+  const target = event.target;
+  
+  if (target.type === 'checkbox' || target.type === 'radio') {
+    console.log(`Checkbox Toggle [${target.name || target.id}]:`, target.checked);
+  }
+});
+
 // --- Helper Functions ---
 
 function poke_list() {
