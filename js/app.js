2eaf095352394fef89204c5bfe9df478c0dc6873
Pagliaa <pagliarin.davide@gmail.com>
2026-08-07 18:27:25 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -908,7 +908,7 @@ function populateSheet(data) {
   if (conTextarea) conTextarea.value = data.conoscenze_personali || '';
 }
 
-async function loadSheetData2() {
+async function loadSheetData2(sheetType) {
   const SUPABASE_URL = 'https://lzppgwqfahqrcgsjyybl.supabase.co';
   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6cHBnd3FmYWhxcmNnc2p5eWJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NjgxMzMsImV4cCI6MjEwMTM0NDEzM30.eTTTCnQ0usjEHnILcc9yboMMI_uC8nS8WdpcA1N_-5k';
 
